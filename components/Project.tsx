6e27534f47a153d5f81af4ac22d07b5d44338053
Paweł Dworniczak <dworniczakpawel@gmail.com>
2024-08-06 11:10:02 +0200

    change color pallete

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,28 +3,31 @@ import styles from "./components.module.scss";
 
 export const Project = ({ project }: { project: TypeProjectFields }) => {
   return (
-    <article className={styles.project}>
-      <header>
-        <h5>{project.name}</h5>
-      </header>
-      <section>{project.description}</section>
-      <section>
+    <>
+      <article className={styles.project}>
         <header>
-          <h6>Tools and technologies:</h6>
+          <h5>{project.name}</h5>
         </header>
-        <span>
-          {project.technologies
-            ? project.technologies.map((technology, index) => (
+        <section>{project.description}</section>
+        <section>
+          <header>
+            <h6>Tools and technologies:</h6>
+          </header>
+          <span>
+            {project.technologies
+              ? project.technologies.map((technology, index) => (
                 <span key={`tech_${index}`}>{technology}</span>
               ))
-            : ""}
-          {project.tools
-            ? project.tools.map((tool, index) => (
+              : ""}
+            {project.tools
+              ? project.tools.map((tool, index) => (
                 <span key={`tool_${index}`}>{tool}</span>
               ))
-            : ""}
-        </span>
-      </section>
-    </article>
+              : ""}
+          </span>
+        </section>
+      </article>
+      <hr />
+    </>
   );
 };
